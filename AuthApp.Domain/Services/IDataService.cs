749397d346d215330b15932c2976c07ea273d143
rokudenas@bk.ru <rokudenas@bk.ru>
2024-03-16 20:55:17 +0300

    ef core

---
+++ AuthApp.Domain/Services/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthApp.Services
+{
+    public interface IDataService<T>
+    {
+        Task<T> Get(T entity);
+
+        Task<T> Create(T entity);
+    }
+}
---
+++ AuthApp.Persistance/AuthAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AuthApp.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+    namespace AuthApp.Persistance
+    {
+        public class AuthAppDbContext : DbContext
+        {
+            public DbSet<User> Users { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AuthAppDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+    }
